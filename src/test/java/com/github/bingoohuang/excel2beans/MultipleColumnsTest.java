489181a14ffcf74de14a67f2eb3bffead70a0508
bingoo <bingoo.huang@gmail.com>
2017-12-05 09:34:12 +0800

    more test

--- src/test/java/com/github/bingoohuang/excel2beans/MultipleColumnsTest.java
+++ src/test/java/com/github/bingoohuang/excel2beans/MultipleColumnsTest.java
@@ -12,16 +12,39 @@ import static com.google.common.truth.Truth.assertThat;
 
 public class MultipleColumnsTest {
     @SneakyThrows
+    @Test public void testAf() {
+        @Cleanup val workbook = ExcelToBeansUtils.getClassPathWorkbook("af-tvplays.xlsx");
+        val beans = new ExcelSheetToBeans(workbook, AfTvPlayBean.class).convert();
+        assertThat(beans).hasSize(1);
+
+        assertThat(beans.get(0)).isEqualTo(AfTvPlayBean.builder()
+                .playName("大风车")
+                .playDescs(Lists.newArrayList("蹲蹲蹲", "跳跳跳", "转转转", "跳跳跳", null))
+                .playUrls(Lists.newArrayList("aaa", null, "ccc", "ddd", "eee"))
+                .build());
+    }
+
+    @Data @Builder
+    public static class AfTvPlayBean {
+        @ExcelColTitle("节目名称")
+        private String playName;
+        @ExcelColTitle("剧集描述")
+        private List<String> playDescs;
+        @ExcelColTitle("URL")
+        private List<String> playUrls;
+    }
+
+    @SneakyThrows
     @Test public void test() {
         @Cleanup val workbook = ExcelToBeansUtils.getClassPathWorkbook("listColumns.xlsx");
         val excelToBeans = new ExcelSheetToBeans(workbook, MultipleColumnsBeanWithTitle.class);
         List<MultipleColumnsBeanWithTitle> beans = excelToBeans.convert();
         assertThat(beans).hasSize(4);
 
-        assertThat(beans.get(0).getRowNum()).isEqualTo(6);
-        assertThat(beans.get(1).getRowNum()).isEqualTo(7);
-        assertThat(beans.get(2).getRowNum()).isEqualTo(8);
-        assertThat(beans.get(3).getRowNum()).isEqualTo(9);
+        assertThat(beans.get(0).getRowNum()).isEqualTo(7);
+        assertThat(beans.get(1).getRowNum()).isEqualTo(8);
+        assertThat(beans.get(2).getRowNum()).isEqualTo(9);
+        assertThat(beans.get(3).getRowNum()).isEqualTo(10);
 
         assertThat(beans.get(0)).isEqualTo(MultipleColumnsBeanWithTitle.builder().memberName("张小凡")
                 .mobiles(Lists.newArrayList(null, "18795952311", "18795952311", "18795952311"))
